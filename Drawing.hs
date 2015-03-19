c5392c686b9d0c6d5b7143cef64525685c1bccc3
ToF- <cthibaut@octo.com>
2015-03-19 20:41:32 +0100

    viewport for several lines

--- Drawing.hs
+++ Drawing.hs
@@ -7,7 +7,12 @@ data ViewPort = ViewPort Double Double Double Double
 type Line = [Point]
 type Point = (Double,Double)
 
+vMax = 100000
+vMin = -vMax
+
 viewPort :: [Line] -> ViewPort
-viewPort [[(x0,y0),(x1,y1)]] = ViewPort (x0-xMargin) (y0-yMargin) (x1+xMargin) (y1+yMargin)
-    where xMargin = abs (x1 - x0) * 0.05
-          yMargin = abs (y1 - y0) * 0.05  
+viewPort lines = ViewPort (x0-marginX) (y0-marginY) (x1+marginX) (y1+marginY)
+    where marginX = abs (x1 - x0) * 0.05
+          marginY = abs (y1 - y0) * 0.05  
+          (x0,y0,x1,y1) = foldr findLimits (vMax,vMax,vMin,vMin) $ concat lines
+          findLimits (x,y) (xMin,yMin,xMax,yMax) = (min xMin x, min yMin y, max xMax x, max yMax y)
--- Specs.hs
+++ Specs.hs
@@ -8,6 +8,10 @@ main :: IO ()
 main = hspec $ do
     describe "viewPort" $ do
         it "should find a viewPort for the rendering of a unique line" $ do
-        viewPort [[(-1.0,-2.0),(2.0,2.0)]] `shouldBe` ViewPort (-1.15) (-2.2) (2.15) (2.2)  
-        viewPort [[(1.0,1.0),(11.0,11.0)]] `shouldBe` ViewPort 0.5 0.5 11.5 11.5
+            viewPort [[(-1.0,-2.0),(2.0,2.0)]] `shouldBe` ViewPort (-1.15) (-2.2) (2.15) (2.2)  
+            viewPort [[(1.0,1.0),(11.0,11.0)]] `shouldBe` ViewPort 0.5 0.5 11.5 11.5
+
+        it "should find a viewPort for the rendering of many lines" $ do
+            viewPort [[(-1.0,-2.0),(2.0,2.0)],[(1.0,1.0),(11.0,11.0)]] 
+            `shouldBe` ViewPort (-1.6) (-2.65) (11.6) (11.65)
  
